0ab1702d051c297a1b23154818de0810c3f74809
BothSann <boththann76@gmail.com>
2025-03-13 19:40:27 +0700

    Replace RedirectAdminIfAuthenticated with RedirectIfAuthenticated in middleware configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Middleware\RedirectAdminIfAuthenticated;
-use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
+use App\Http\Middleware\RedirectIfAuthenticated;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            "guest" => RedirectAdminIfAuthenticated::class,
+            "guest" => RedirectIfAuthenticated::class,
             
         ]);
     })
