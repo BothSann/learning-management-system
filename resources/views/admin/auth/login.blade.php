bb7723087c3d2cd8e5e4f1762d6901230208b21e
BothSann <boththann76@gmail.com>
2025-03-13 22:41:49 +0700

    Update password reset link in admin login view to use admin route

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -40,7 +40,7 @@
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="text-sm text-gray-600 underline rounded-md hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    href="{{ route('password.request') }}">
+                    href="{{ route('admin.password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
